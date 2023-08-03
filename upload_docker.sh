57d35e9a5f3b95fdd35ca0b1c10670594c72c112
EC2 Default User <ec2-user@ip-172-31-35-58.ec2.internal>
2023-08-03 07:46:02 +0000

    tesst

--- upload_docker.sh
+++ upload_docker.sh
@@ -1,16 +1,9 @@
-# !/usr/bin/env bash
-# This file tags and uploads an image to Docker Hub
+#!/usr/bin/env bash
+# This script tags and uploads a Docker image to Docker Hub.
 
-# Assumes that an image is built via `run_docker.sh`
-
-# Step 1:
-# Create docker_path (username)
 docker_path=longnt2
 
-
-# Step 2:  
-# Authenticate & tag
-# echo "Docker ID and Image: $docker_path"
+# Authenticate & tag the Docker image
 docker login
 
 # Check if the image is already tagged and remove it if necessary
@@ -27,18 +20,22 @@ fi
 
 # Get information about the image by substring 
 image_info=$(docker image list | grep 'project-ml' | xargs)
+
 # Extract the image name from the image information
 image_name=$(echo "$image_info" | cut -f 1 -d " ")
+
+
 # Extract the image tag from the image information
 image_tag=$(echo "$image_info" | cut -f 2 -d " ")
+
 
 # Tag the image with the specified repository and tag
 docker image tag "$image_name:$image_tag" "$docker_path/$image_name:$image_tag"
 
+
 # List the tagged image
 docker image list --filter=reference="$docker_path/project-ml"
 
-# Step 3:
-# Push image to a docker repository
+
 # Push the tagged image to the Docker repository
 docker image push "$docker_path/project-ml:$image_tag"
